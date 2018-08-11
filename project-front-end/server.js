c513253403f48a023c027eac0ba6f8194a3a8ce4
Ryan <green.ry@husky.neu.edu>
2018-08-11 18:48:26 -0400

    tryin shit

--- project-front-end/server.js
+++ project-front-end/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/simple-blog-app-angular'));
+app.use(express.static('./dist/pan-free-app'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('./dist/simple-blog-app-angular/index.html'));
+  res.sendFile(path.join('./dist/pan-free-app/index.html'));
 });
 app.listen(process.env.PORT || 8080);
