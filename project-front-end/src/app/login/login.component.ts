c3876af6ab25f5978af187b5b484eb3d1555a0b7
lockhartc <lockhart.c@husky.neu.edu>
2018-08-11 15:37:41 -0400

    began updating the front end for: user, home, login, register, profile, pantry, recipe

--- project-front-end/src/app/login/login.component.ts
+++ project-front-end/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {UserServiceClient} from '../services/user.service.client';
-let selfReference
+// import {Router} from '@angular/router';
+// import {UserServiceClient} from '../services/user.service.client';
+// let selfReference
 
 @Component({
   selector: 'app-login',
@@ -11,25 +11,28 @@ let selfReference
 
 export class LoginComponent implements OnInit {
 
-  username;
-  password;
-  login(username, password) {
-    console.log([username, password]);
-    this.userService.findUserByCredentials(username, password)
-      .then(function (user) {
-        if (user == null) {
-          alert('Uth oh, it seems your username or password doesn\'t work!' +
-            ' Please, try again.');
-        } else {
-          selfReference.userService.login(username, password)
-            .then(() => selfReference.router.navigate( ['profile']));
-        }
-      });
+  // username;
+  // password;
+  // login(username, password) {
+  //   console.log([username, password]);
+  //   this.userService.findUserByCredentials(username, password)
+  //     .then(function (user) {
+  //       if (user == null) {
+  //         alert('Uth oh, it seems your username or password doesn\'t work!' +
+  //           ' Please, try again.');
+  //       } else {
+  //         selfReference.userService.login(username, password)
+  //           .then(() => selfReference.router.navigate( ['profile']));
+  //       }
+  //     });
+  //
+  // }
 
-  }
-
-  constructor(private router: Router, private userService: UserServiceClient) {
-    selfReference = this;
+  constructor(
+    // private router: Router,
+    // private userService: UserServiceClient
+  ) {
+    // selfReference = this;
   }
 
   ngOnInit() {
