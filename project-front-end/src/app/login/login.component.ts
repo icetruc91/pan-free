06b08e83fae6d816fba4216535214475748b9aa9
Ryan <green.ry@husky.neu.edu>
2018-08-11 17:04:40 -0400

    login wiring started

--- project-front-end/src/app/login/login.component.ts
+++ project-front-end/src/app/login/login.component.ts
@@ -14,21 +14,24 @@ let selfReference
 export class LoginComponent implements OnInit {
 
   logn;
+  pswd;
 
   login(logn) {
     console.log([logn]);
     this.personService.findPersonByLogin(logn)
-      .then(function (logn) {
-        if (logn == null) {
-          alert('Uth oh, it seems your username or password doesn\'t work!' +
-            ' Please, try again.');
+      .then(function (user) {
+        if (user == null) {
+          alert('Uth oh, it seems your username or password doesn\'t exist!' +
+            ' Please, try again or sign up at the Register page');
+        } else if (user.password != this.pswd) {
+            alert('Woops... Your password does not match the given username. Try again!');
         } else {
-          selfReference.personService.login(logn)
-            .then(() => selfReference.router.navigate( ['profile']));
+          // selfReference.personService.login(logn)
+          //   .then(() => selfReference.router.navigate(['profile']));
         }
       });
 
-
+  }
 
   constructor(private router: Router, private personService: PersonServiceClient) {
     selfReference = this;
